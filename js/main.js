9ba7ea361b74cdd75730471616528ef932c4b65a
Roman <65686226+morozit@users.noreply.github.com>
2020-10-13 14:40:44 +0300

    update

--- js/main.js
+++ js/main.js
@@ -4,15 +4,18 @@ const refs = {
   burger: document.querySelector("#burger"),
   menu: document.querySelector("#menu"),
   mobileMenu: document.querySelector("#mobile-menu"),
+  modalsWrapper: document.querySelector("#modals-wrapper"),
 };
 
 refs.burger.addEventListener("click", toggleBurger);
 refs.mobileMenu.addEventListener("click", closeBurger);
+refs.modalsWrapper.addEventListener("click", closeBurger);
 
 function toggleBurger(event) {
   event.preventDefault();
   event.currentTarget.classList.toggle("active");
   refs.mobileMenu.classList.toggle("active");
+  refs.modalsWrapper.classList.toggle("active");
   refs.menu.classList.toggle("active");
   document.body.classList.toggle("lock");
 }
@@ -21,5 +24,6 @@ function closeBurger(event) {
   refs.burger.classList.remove("active");
   refs.menu.classList.remove("active");
   refs.mobileMenu.classList.remove("active");
+  refs.modalsWrapper.classList.remove("active");
   document.body.classList.remove("lock");
 }
