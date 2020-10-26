d9fc698c26ce6ad2a4fbf22f1237178970a65059
Roman <65686226+morozit@users.noreply.github.com>
2020-10-26 16:59:15 +0200

    up

--- js/main.js
+++ js/main.js
@@ -1,29 +1,54 @@
-"use strict";
+// "use strict";
 
-const refs = {
-  burger: document.querySelector("#burger"),
-  menu: document.querySelector("#menu"),
-  mobileMenu: document.querySelector("#mobile-menu"),
-  modalsWrapper: document.querySelector("#modals-wrapper"),
-};
+// const refs = {
+//   burger: document.querySelector("#burger"),
+//   menu: document.querySelector("#menu"),
+//   mobileMenu: document.querySelector("#mobile-menu"),
+//   modalsWrapper: document.querySelector("#modals-wrapper"),
+// };
 
-refs.burger.addEventListener("click", toggleBurger);
-refs.mobileMenu.addEventListener("click", closeBurger);
-refs.modalsWrapper.addEventListener("click", closeBurger);
+// refs.burger.addEventListener("click", toggleBurger);
+// refs.mobileMenu.addEventListener("click", closeBurger);
+// refs.modalsWrapper.addEventListener("click", closeBurger);
 
-function toggleBurger(event) {
-  event.preventDefault();
-  event.currentTarget.classList.toggle("active");
-  refs.mobileMenu.classList.toggle("active");
-  refs.modalsWrapper.classList.toggle("active");
-  refs.menu.classList.toggle("active");
-  document.body.classList.toggle("lock");
-}
+// function toggleBurger(event) {
+//   event.preventDefault();
+//   event.currentTarget.classList.toggle("active");
+//   refs.mobileMenu.classList.toggle("active");
+//   refs.modalsWrapper.classList.toggle("active");
+//   refs.menu.classList.toggle("active");
+//   document.body.classList.toggle("lock");
+// }
 
-function closeBurger(event) {
-  refs.burger.classList.remove("active");
-  refs.menu.classList.remove("active");
-  refs.mobileMenu.classList.remove("active");
-  refs.modalsWrapper.classList.remove("active");
-  document.body.classList.remove("lock");
-}
+// function closeBurger(event) {
+//   refs.burger.classList.remove("active");
+//   refs.menu.classList.remove("active");
+//   refs.mobileMenu.classList.remove("active");
+//   refs.modalsWrapper.classList.remove("active");
+//   document.body.classList.remove("lock");
+// }
+
+
+
+// SLIDER
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters  
+  // loop: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
